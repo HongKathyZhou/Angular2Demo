0ec11a3d24dfba4aa5a62c44d6486ee2ed0e0740
HongKathyZhou <zhou.hong@gmail.com>
2016-07-28 15:12:51 -0400

    Update in-memory-data.service.ts

--- scripts/in-memory-data.service.ts
+++ scripts/in-memory-data.service.ts
@@ -1,31 +1,31 @@
 export class InMemoryDataService {
   createDb() {
     let patients = [
-      {id: 11, name: 'Mr. Nice'},
-      {id: 12, name: 'Narco'},
-      {id: 13, name: 'Bombasto'},
-      {id: 14, name: 'Celeritas'},
-      {id: 15, name: 'Magneta'},
-      {id: 16, name: 'RubberMan'},
-      {id: 17, name: 'Dynama'},
-      {id: 18, name: 'Dr IQ'},
-      {id: 19, name: 'Magma'},
-      {id: 20, name: 'Tornado'}
+      {id: 1, name: 'Bob Joe'},
+      {id: 2, name: 'Bill T'},
+      {id: 3, name: 'Ice Man'},
+      {id: 4, name: 'Charles'},
+      {id: 5, name: 'Magic'},
+      {id: 6, name: 'Chunky'},
+      {id: 7, name: 'Silk Teddy'},
+      {id: 8, name: 'Kid Lupus'},
+      {id: 9, name: 'Mocha Rose'},
+      {id: 10, name: 'Molly Popper'}
     ];
 
     let funds = [
-      {id: 1, name: 'General Pediatrics', ticker: 'AMCR1'},
-      {id: 2, name: 'Pediatric Cardiology, Pediatrics', ticker: 'AMCR2'},
-      {id: 3, name: 'Pediatrics/Neonatal Care Nursing', ticker: 'AMCR3'},
-      {id: 4, name: 'Pediatric Ophthalmology', ticker: 'FDAF1'},
-      {id: 5, name: 'Adolescent Medicine, Pediatrics', ticker: 'FDAF2'},
-      {id: 6, name: 'Child Abuse Pediatrics', ticker: 'TRPEF1'},
-      {id: 7, name: 'Developmental-Behavioral Pediatrics', ticker: 'TRPEB'},
-      {id: 8, name: 'Diagnostic Laboratory Immunology', ticker: 'TAACB'},
-      {id: 9, name: 'Medical Toxicology', ticker: 'TAACI'},
-      {id: 10, name: 'Neonatal-Perinatal Medicine, Pediatrics', ticker: 'VANGI'},
-      {id: 11, name: 'Neurodevelopmental Disabilities', ticker: 'VANGI'},
-      {id: 12, name: 'Pediatric Dermatology', ticker: 'VANGI'}
+      {id: 1, name: 'General Pediatrics', ticker: 'GP'},
+      {id: 2, name: 'Pediatric Cardiology, Pediatrics', ticker: 'PCP'},
+      {id: 3, name: 'Pediatrics/Neonatal Care Nursing', ticker: 'PNCN'},
+      {id: 4, name: 'Pediatric Ophthalmology', ticker: 'PO'},
+      {id: 5, name: 'Adolescent Medicine, Pediatrics', ticker: 'AMP'},
+      {id: 6, name: 'Child Abuse Pediatrics', ticker: 'CAP'},
+      {id: 7, name: 'Developmental-Behavioral Pediatrics', ticker: 'DBP'},
+      {id: 8, name: 'Diagnostic Laboratory Immunology', ticker: 'DLI'},
+      {id: 9, name: 'Medical Toxicology', ticker: 'MT'},
+      {id: 10, name: 'Neonatal-Perinatal Medicine, Pediatrics', ticker: 'NMP'},
+      {id: 11, name: 'Neurodevelopmental Disabilities', ticker: 'ND'},
+      {id: 12, name: 'Pediatric Dermatology', ticker: 'PD'}
     ];
     return {patients, funds};
   }
